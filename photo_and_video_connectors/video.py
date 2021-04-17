f8550614c3ce2261f359da10bb67d9d2dc087492
sebastiandres <sebastiandres@gmail.com>
2021-04-16 23:34:01 -0400

    update version

--- photo_and_video_connectors/video.py
+++ photo_and_video_connectors/video.py
@@ -6,25 +6,25 @@ def setup_properties(video_type="", height=480, width=600):
     Starts the video stream ...    
     """
     # Dynamically declarate the global variables in javascript
-    js_1 = f"var DEFAULT_VIDEO_TYPE='{video_type}';"
-    js_2 = f"var DEFAULT_HEIGHT='{height}';"
-    js_3 = f"var DEFAULT_WIDTH='{width}';"
-    js_code = Javascript("\n".join([js_1, js_2, js_3]))
-    display(js_code)
+    js_1 = f"var VIDEO_TYPE = '{video_type}';"
+    js_2 = f"var HEIGHT = '{height}';"
+    js_3 = f"var WIDTH = '{width}';"
+    js_txt = "\n".join([js_1, js_2, js_3, "\n\n"])
     # Load the shared javascript functions
     # This requires VIDEO_TYPE, HEIGHT, WIDTH
     with open("../js_code/shared.js") as fh:
-        js_txt = "".join(fh.readlines())
-    js_code = Javascript(js_txt)
-    display(js_code)
+        js_aux = "".join(fh.readlines())
+    js_txt = js_txt + "\n" + js_aux
     # Load video.js
     # This requires VIDEO_TYPE, HEIGHT, WIDTH
     with open("../js_code/video.js") as fh:
-        js_txt = "".join(fh.readlines())
-    js_code = Javascript(js_txt)
-    display(js_code)
+        js_aux = "".join(fh.readlines())
+    js_txt = js_txt + "\n" + js_aux
     # Launch something, if needed
-    display(Javascript('add_choice_div("video");'))
+    js_txt = js_txt + "\n" + 'add_choice_div("video");'
+    with open("js_code.txt","w") as fh:
+        fh.write(js_txt)
+    display(Javascript(js_txt))
     return
     
 def update_frame(bbox=""):
