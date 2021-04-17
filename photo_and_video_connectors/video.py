6debc2b8c73ac2ce5bdb112afe523bb571a9409f
sebastiandres <sebastiandres@gmail.com>
2021-04-17 14:14:53 -0400

    updated relative path to import code

--- photo_and_video_connectors/video.py
+++ photo_and_video_connectors/video.py
@@ -1,10 +1,13 @@
 from IPython.display import display, Javascript
 #from google.colab.output import eval_js
+import os
 
 def setup_properties(video_type="", height=480, width=600):
     """
     Starts the video stream ...    
     """
+    # Get file path
+    cwd = os.path.dirname(__file__), ".")
     # Dynamically declarate the global variables in javascript
     js_1 = f"var VIDEO_TYPE = '{video_type}';"
     js_2 = f"var HEIGHT = '{height}';"
@@ -12,12 +15,12 @@ def setup_properties(video_type="", height=480, width=600):
     js_txt = "\n".join([js_1, js_2, js_3, "\n\n"])
     # Load the shared javascript functions
     # This requires VIDEO_TYPE, HEIGHT, WIDTH
-    with open("../js_code/shared.js") as fh:
+    with open(os.path.join(cwd, "js_code/shared.js")) as fh:
         js_aux = "".join(fh.readlines())
     js_txt = js_txt + "\n" + js_aux
     # Load video.js
     # This requires VIDEO_TYPE, HEIGHT, WIDTH
-    with open("../js_code/video.js") as fh:
+    with open(os.path.join(cwd, "js_code/shared.js")) as fh:
         js_aux = "".join(fh.readlines())
     js_txt = js_txt + "\n" + js_aux
     # Launch something, if needed
