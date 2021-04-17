078906511659e76bfae0661006237104f1d385d3
sebastiandres <sebastiandres@gmail.com>
2021-04-16 21:19:15 -0400

    another another try

--- photo_and_video_connectors/__init__.py
+++ photo_and_video_connectors/__init__.py
@@ -1,4 +1,8 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
+
 from .version_file import version_number as version
 
 ### Change it if you change the names of files or classes
-from py_code import video
+from .py_code import video
